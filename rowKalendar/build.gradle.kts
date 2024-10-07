973c4a6df7aac3e8230c18d9f27fdaee157626c6
chouaibMo <chouaib@purchasely.com>
2024-10-08 01:26:21 +0200

    Bump library version

--- rowKalendar/build.gradle.kts
+++ rowKalendar/build.gradle.kts
@@ -56,7 +56,7 @@ mavenPublishing {
     coordinates(
         artifactId = "rowkalendar",
         groupId = "io.github.chouaibmo",
-        version = "0.0.1"
+        version = "0.0.2"
     )
 
     pom{
